af3efe75ffa70c6a8c8701eda6c94edcf73f4269
Max Gallo <max.gallo@dazn.com>
2023-08-02 23:00:09 +0200

    feat: using murmurhash algorithm for bucketing

--- Sources/FeaturevisorSDK/bucket.swift
+++ Sources/FeaturevisorSDK/bucket.swift
@@ -1,13 +1,13 @@
 import Foundation
+import MurmurHash_Swift
 
 let HASH_SEED: UInt32 = 1
-let MAX_HASH_VALUE = UInt32.max // 2^32 - 1
+let MAX_HASH_VALUE: Double = pow(2, 32) // 2^32
 let MAX_BUCKETED_NUMBER = 100000 // 100% * 1000 to include three decimal places in the same integer value
 
 func getBucketedNumber(bucketKey: String) -> Int {
-  var hasher = Hasher()
-  hasher.combine(bucketKey)
-  let hashValue = UInt32(abs(hasher.finalize())) // Absolute value to prevent negative numbers
-  let ratio = Double(hashValue) / Double(MAX_HASH_VALUE)
+  let hashValue = MurmurHash3.x86_32.digest(bucketKey, seed: HASH_SEED)
+  let ratio = Double(hashValue) / MAX_HASH_VALUE
+
   return Int(floor(ratio * Double(MAX_BUCKETED_NUMBER)))
 }
