e36c47dfd48997de5b83105cd1bd80e7430b7285
Marcin Polak <mpolak87@gmail.com>
2023-10-03 13:48:06 +0200

    chore: Use https for public dependencies (#40)

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
   dependencies: [
     // Dependencies declare other packages that this package depends on.
     // .package(url: /* package url */, from: "1.0.0"),
-    .package(url: "git@github.com:daisuke-t-jp/MurmurHash-Swift.git", from: "1.1.1"),
+    .package(url: "https://github.com/daisuke-t-jp/MurmurHash-Swift.git", from: "1.1.1"),
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
