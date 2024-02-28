0864c747ae28e0f1eb55e75cb41bda14f1d82fb2
Marcin Polak <marcin.polak@dazn.com>
2024-02-28 11:42:10 +0100

    Add new executable target

--- Package.swift
+++ Package.swift
@@ -4,50 +4,62 @@
 import PackageDescription
 
 let package = Package(
-  name: "FeaturevisorSDK",
-  products: [
-    // Products define the executables and libraries produced by a package, and make them visible to other packages.
-    .library(
-      name: "FeaturevisorSDK",
-      targets: ["FeaturevisorSDK"]
-    ),
-    .library(
-      name: "FeaturevisorTypes",
-      targets: ["FeaturevisorTypes"]
-    ),
-  ],
-  dependencies: [
-    // Dependencies declare other packages that this package depends on.
-    // .package(url: /* package url */, from: "1.0.0"),
-    .package(url: "https://github.com/daisuke-t-jp/MurmurHash-Swift.git", from: "1.1.1"),
-  ],
-  targets: [
-    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-    // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-    .target(
-      name: "FeaturevisorTypes",
-      dependencies: []
-    ),
-    .target(
-      name: "FeaturevisorSDK",
-      dependencies: [
-        "FeaturevisorTypes",
-        "MurmurHash-Swift"
-      ]
-    ),
-    .testTarget(
-      name: "FeaturevisorSDKTests",
-      dependencies: [
-        "FeaturevisorSDK"
-      ]
-    ),
-    .testTarget(
-        name: "FeaturevisorTypesTests",
-        dependencies: [
-            "FeaturevisorTypes"
-        ],
-        resources: [
-            .process("JSONs")
-        ]),
-  ]
+    name: "FeaturevisorSDK",
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "FeaturevisorSDK",
+            targets: ["FeaturevisorSDK"]
+        ),
+        .library(
+            name: "FeaturevisorTypes",
+            targets: ["FeaturevisorTypes"]
+        ),
+        .executable(
+            name: "Featurevisor-swift",
+            targets: ["FeaturevisorTestRunner"]
+        ),
+    ],
+    dependencies: [
+        // Dependencies declare other packages that this package depends on.
+        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/daisuke-t-jp/MurmurHash-Swift.git", from: "1.1.1")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "FeaturevisorTypes",
+            dependencies: []
+        ),
+        .target(
+            name: "FeaturevisorSDK",
+            dependencies: [
+                "FeaturevisorTypes",
+                "MurmurHash-Swift",
+            ]
+        ),
+        .executableTarget(
+            name: "FeaturevisorTestRunner",
+            dependencies: [
+
+            ],
+            path: "Sources/FeaturevisorTestRunner"
+        ),
+        .testTarget(
+            name: "FeaturevisorSDKTests",
+            dependencies: [
+                "FeaturevisorSDK"
+            ]
+        ),
+        .testTarget(
+            name: "FeaturevisorTypesTests",
+            dependencies: [
+                "FeaturevisorTypes"
+            ],
+            resources: [
+                .process("JSONs")
+            ]
+        ),
+    ]
 )
---
+++ Sources/FeaturevisorTestRunner/FeaturevisorTestRunner.swift
@@ -0,0 +1,13 @@
+//
+// Created by Marcin Polak on 28/02/2024.
+//
+
+import Foundation
+
+@main
+struct FeaturevisorTestRunner {
+
+    static func main() {
+        print("Hello my lovely Featurevisor Swift Test Runner!")
+    }
+}
