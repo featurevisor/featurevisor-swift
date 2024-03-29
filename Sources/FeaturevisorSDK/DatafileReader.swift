9bf0aa863f19e78af843e6bbc21f363851e337d4
Marcin Polak <marcin.polak@dazn.com>
2024-03-29 19:39:16 +0100

    refactor: performance tweaks under `DatafileReader`

--- Sources/FeaturevisorSDK/DatafileReader.swift
+++ Sources/FeaturevisorSDK/DatafileReader.swift
@@ -3,16 +3,22 @@ import FeaturevisorTypes
 public class DatafileReader {
     let schemaVersion: String
     let revision: String
-    let attributes: [Attribute]
-    let segments: [Segment]
-    let features: [Feature]
+    let attributes: [AttributeKey: Attribute]
+    let segments: [SegmentKey: Segment]
+    let features: [FeatureKey: Feature]
 
     init(datafileContent: DatafileContent) {
         self.schemaVersion = datafileContent.schemaVersion
         self.revision = datafileContent.revision
-        self.segments = datafileContent.segments
-        self.attributes = datafileContent.attributes
-        self.features = datafileContent.features
+        self.segments = Dictionary(
+            uniqueKeysWithValues: datafileContent.segments.map { ($0.key, $0) }
+        )
+        self.attributes = Dictionary(
+            uniqueKeysWithValues: datafileContent.attributes.map { ($0.key, $0) }
+        )
+        self.features = Dictionary(
+            uniqueKeysWithValues: datafileContent.features.map { ($0.key, $0) }
+        )
     }
 
     public func getRevision() -> String {
@@ -24,18 +30,18 @@ public class DatafileReader {
     }
 
     public func getAllAttributes() -> [Attribute] {
-        return self.attributes
+        return Array(attributes.values)
     }
 
     public func getAttribute(_ attributeKey: AttributeKey) -> Attribute? {
-        return self.attributes.first(where: { $0.key == attributeKey })
+        return self.attributes[attributeKey]
     }
 
     public func getSegment(_ segmentKey: SegmentKey) -> Segment? {
-        return segments.first(where: { $0.key == segmentKey })
+        return segments[segmentKey]
     }
 
     public func getFeature(_ featureKey: FeatureKey) -> Feature? {
-        return features.first(where: { $0.key == featureKey })
+        return features[featureKey]
     }
 }
